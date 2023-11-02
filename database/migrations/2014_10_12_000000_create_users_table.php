9e443e71b091013a353700f19940116e70805139
Programacion <programacion@192.168.0.217>
2023-11-02 13:31:47 -0600

    login with auth

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,6 +19,11 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->integer('type_user');
+            $table->string('curp');
+            $table->string('rfc');
+            $table->date('date_birthday');
+            // $table->date('id_area');
             $table->rememberToken();
             $table->timestamps();
         });
