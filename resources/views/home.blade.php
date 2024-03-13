e0891fe16af723b8a2ab0ede1678daf79791a163
ARTEN-Digital <soporte@artendigital.com.mx>
2024-03-13 12:15:17 -0600

    pruebas git

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -14,7 +14,7 @@
     <!-- Styles -->
     <link rel="stylesheet" href="{{ mix('css/app.css') }}">
 
-
+ 
     <link rel="stylesheet" href="/css/introjs.css">
     <script src="/js/intro.js"></script>
 
