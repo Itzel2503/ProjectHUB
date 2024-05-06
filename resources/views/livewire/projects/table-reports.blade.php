f18590841d59eca3a66690b60cc281c2c089d87c
Programacion <itzel.d.cruz.aguilar@hotmail.com>
2024-05-06 13:37:18 -0600

    error reportes

--- resources/views/livewire/projects/table-reports.blade.php
+++ resources/views/livewire/projects/table-reports.blade.php
@@ -298,7 +298,7 @@
                     @foreach ($reports as $report)
                         <tr class="trTable">
                             <td class="px-4 py-2 relative">
-                                <div wire:click="showActivity({{ $report->id }})"
+                                <div wire:click="showReport({{ $report->id }})"
                                     class="flex cursor-pointer flex-col items-center text-center">
                                     <p class="mb-2 text-justify text-xs font-semibold">{{ $report->tittle }}</p>
                                     @if (!empty($report->content))
