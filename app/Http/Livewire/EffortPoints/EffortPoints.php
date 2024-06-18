819e9ef1a4376d92375507eab35ce295efcf5cf7
Programacion <itzel.d.cruz.aguilar@hotmail.com>
2024-06-18 15:08:12 -0600

    puntos de esfuerzo

--- app/Http/Livewire/EffortPoints/EffortPoints.php
+++ app/Http/Livewire/EffortPoints/EffortPoints.php
@@ -14,67 +14,95 @@ class EffortPoints extends Component
     protected $listeners = ['setDate'];
     // Fecha
     public $dateRange;
-    public $startDate, $endDate;
+    public $startDate, $endDate, $starMonth, $endMonth;
     public $qty = 7;
 
     public function render()
     {
-        if ($this->startDate == null && $this->startDate == null) {
-            // Establecer valores iniciales para las fechas en formato YYYY-MM-DD
-            // $this->startDate = Carbon::now()->startOfWeek()->format('Y-m-d'); // Lunes de esta semana
-            // $this->endDate = Carbon::now()->endOfWeek()->format('Y-m-d'); // Domingo de esta semana
-            $this->startDate = Carbon::now()->startOfMonth()->format('Y-m-d'); // Primer día del mes actual
-            $this->endDate = Carbon::now()->endOfMonth()->format('Y-m-d'); // Último día del mes actual
-        }
-        
-        $reportsSubquery = Report::select(
-                'delegate_id',
-                DB::raw("SUM(CASE WHEN state = 'Abierto' THEN points ELSE 0 END) as report_points_abierto"),
-                DB::raw("SUM(CASE WHEN state = 'Proceso' THEN points ELSE 0 END) as report_points_proceso"),
-                DB::raw("SUM(CASE WHEN state = 'Conflicto' THEN points ELSE 0 END) as report_points_conflicto"),
-                DB::raw("SUM(CASE WHEN state = 'Resuelto' THEN points ELSE 0 END) as report_points_resuelto")
-            )
-            ->whereBetween('updated_at', [$this->startDate, $this->endDate])
-            ->groupBy('delegate_id');
+        $this->startDate = Carbon::now()->startOfWeek()->format('Y-m-d'); // Lunes de esta semana
+        $this->endDate = Carbon::now()->endOfWeek()->format('Y-m-d'); // Domingo de esta semana
 
-        $activitiesSubquery = Activity::select(
-                'delegate_id',
-                DB::raw("SUM(CASE WHEN state = 'Abierto' THEN points ELSE 0 END) as activity_points_abierto"),
-                DB::raw("SUM(CASE WHEN state = 'Proceso' THEN points ELSE 0 END) as activity_points_proceso"),
-                DB::raw("SUM(CASE WHEN state = 'Conflicto' THEN points ELSE 0 END) as activity_points_conflicto"),
-                DB::raw("SUM(CASE WHEN state = 'Resuelto' THEN points ELSE 0 END) as activity_points_resuelto")
-            )
-            ->whereBetween('updated_at', [$this->startDate, $this->endDate])
+        $this->starMonth = Carbon::now()->startOfMonth()->format('Y-m-d'); // Primer día del mes actual
+        $this->endMonth = Carbon::now()->endOfMonth()->format('Y-m-d'); // Último día del mes actual
+        // Subconsulta de Reports por mes
+        $reportsMonthly = Report::select(
+            'delegate_id',
+            DB::raw("SUM(points) as total_points_reports")
+        )
+            ->whereIn('state', ['Abierto', 'Proceso', 'Conflicto'])
+            ->where(function ($query) {
+                $query->whereBetween('updated_at', [$this->starMonth, $this->endMonth])
+                    ->orWhereBetween('expected_date', [$this->starMonth, $this->endMonth]);
+            })
             ->groupBy('delegate_id');
-
+        // Subconsulta de Activities por mes
+        $activitiesMonthly = Activity::select(
+            'delegate_id',
+            DB::raw("SUM(points) as total_points_activities")
+        )
+            ->whereIn('state', ['Abierto', 'Proceso', 'Conflicto'])
+            ->where(function ($query) {
+                $query->whereBetween('updated_at', [$this->starMonth, $this->endMonth])
+                    ->orWhereBetween('expected_date', [$this->starMonth, $this->endMonth]);
+            })
+            ->groupBy('delegate_id');
+        // Subconsulta de Reports por semana
+        $reportsWeekly = Report::select(
+            'delegate_id',
+            DB::raw("SUM(CASE WHEN state = 'Abierto' THEN points ELSE 0 END) as report_abierto"),
+            DB::raw("SUM(CASE WHEN state = 'Proceso' THEN points ELSE 0 END) as report_proceso"),
+            DB::raw("SUM(CASE WHEN state = 'Conflicto' THEN points ELSE 0 END) as report_conflicto"),
+            DB::raw("SUM(CASE WHEN state = 'Resuelto' THEN points ELSE 0 END) as report_resuelto")
+        )
+            ->where(function ($query) {
+                $query->whereBetween('updated_at', [$this->startDate, $this->endDate])
+                    ->orWhereBetween('expected_date', [$this->startDate, $this->endDate]);
+            })
+            ->groupBy('delegate_id');
+        // Subconsulta de Activities por semana
+        $activitiesWeekly = Activity::select(
+            'delegate_id',
+            DB::raw("SUM(CASE WHEN state = 'Abierto' THEN points ELSE 0 END) as activity_abierto"),
+            DB::raw("SUM(CASE WHEN state = 'Proceso' THEN points ELSE 0 END) as activity_proceso"),
+            DB::raw("SUM(CASE WHEN state = 'Conflicto' THEN points ELSE 0 END) as activity_conflicto"),
+            DB::raw("SUM(CASE WHEN state = 'Resuelto' THEN points ELSE 0 END) as activity_resuelto")
+        )
+            ->where(function ($query) {
+                $query->whereBetween('updated_at', [$this->startDate, $this->endDate])
+                    ->orWhereBetween('expected_date', [$this->startDate, $this->endDate]);
+            })
+            ->groupBy('delegate_id');
+        // Consulta principal unificada
         $points = User::select(
-                'users.id',
-                'users.name',
-                'users.effort_points',
-                'report_points_abierto',
-                'report_points_proceso',
-                'report_points_conflicto',
-                'report_points_resuelto',
-                'activity_points_abierto',
-                'activity_points_proceso',
-                'activity_points_conflicto',
-                'activity_points_resuelto'
-            )
-            ->leftJoinSub($reportsSubquery, 'reports', 'users.id', '=', 'reports.delegate_id')
-            ->leftJoinSub($activitiesSubquery, 'activities', 'users.id', '=', 'activities.delegate_id')
+            'users.id',
+            'users.name',
+            'users.effort_points',
+            'reports_weekly.report_abierto',
+            'reports_weekly.report_proceso',
+            'reports_weekly.report_conflicto',
+            'reports_weekly.report_resuelto',
+            'activities_weekly.activity_abierto',
+            'activities_weekly.activity_proceso',
+            'activities_weekly.activity_conflicto',
+            'activities_weekly.activity_resuelto',
+            'reports_monthly.total_points_reports',
+            'activities_monthly.total_points_activities'
+        )
+            ->leftJoinSub($reportsWeekly, 'reports_weekly', 'users.id', '=', 'reports_weekly.delegate_id')
+            ->leftJoinSub($activitiesWeekly, 'activities_weekly', 'users.id', '=', 'activities_weekly.delegate_id')
+            ->leftJoinSub($reportsMonthly, 'reports_monthly', 'users.id', '=', 'reports_monthly.delegate_id')
+            ->leftJoinSub($activitiesMonthly, 'activities_monthly', 'users.id', '=', 'activities_monthly.delegate_id')
             ->where('type_user', '!=', 3)
             ->orderBy('name', 'asc')
             ->get();
-        
+
         foreach ($points as $key => $point) {
             // Puntos por terminar
-            $points_finish_report = $point->report_points_abierto + $point->report_points_proceso + $point->report_points_conflicto + $point->report_points_resuelto;
-            $points_finish_activity = $point->activity_points_abierto +  $point->activity_points_proceso + $point->activity_points_conflicto + $point->activity_points_resuelto;
-            $points_finish = $points_finish_report + $points_finish_activity;
+            $points_finish = $point->total_points_reports + $point->total_points_activities;
             // Puntos por asignar
             $point->points_assigned = $point->effort_points - $points_finish;
             // Avance porcentaje
-            $percentage_progress = $point->report_points_resuelto +  $point->activity_points_resuelto;
+            $percentage_progress = $point->report_resuelto +  $point->activity_resuelto;
             // Evitar división por cero
             if ($point->effort_points != 0) {
                 $advance = $percentage_progress / $point->effort_points;
@@ -85,50 +113,55 @@ class EffortPoints extends Component
             $first_name = explode(' ', trim($point->name))[0];
             // Formatear el nombre con el avance
             $point->name_with_advance = $first_name . ' (' . number_format($advance * 100, 2) . '%)';
+            // Asignar effort_points directamente
+            $point->total_effort_points = $point->effort_points;
         }
-        // Preparar los datos para el gráfico
-        $categories = $points->pluck('name_with_advance')->toArray();
+
         $series = [
             [
                 'name' => 'Resuelto',
-                'data' => $points->map(function($point) {
-                    return $point->report_points_resuelto + $point->activity_points_resuelto;
+                'data' => $points->map(function ($point) {
+                    return $point->report_resuelto + $point->activity_resuelto;
                 })->toArray(),
             ],
             [
                 'name' => 'Proceso',
-                'data' => $points->map(function($point) {
-                    return $point->report_points_proceso + $point->activity_points_proceso;
+                'data' => $points->map(function ($point) {
+                    return $point->report_proceso + $point->activity_proceso;
                 })->toArray(),
             ],
             [
                 'name' => 'Conflicto',
-                'data' => $points->map(function($point) {
-                    return $point->report_points_conflicto + $point->activity_points_conflicto;
+                'data' => $points->map(function ($point) {
+                    return $point->report_conflicto + $point->activity_conflicto;
                 })->toArray(),
             ],
             [
                 'name' => 'Abierto',
-                'data' => $points->map(function($point) {
-                    return $point->report_points_abierto + $point->activity_points_abierto;
+                'data' => $points->map(function ($point) {
+                    return $point->report_abierto + $point->activity_abierto;
                 })->toArray(),
             ],
             [
                 'name' => 'Asignar',
-                'data' => $points->map(function($point) {
+                'data' => $points->map(function ($point) {
                     return $point->points_assigned;
                 })->toArray(),
             ]
         ];
-        // contar los dias
-        $carbon = Carbon::now();
-        // Obtener el número de días en el mes actual
-        $diasEnElMes = $carbon->daysInMonth;
-        $days_filtred = $diasEnElMes;
+        // Preparar los datos para el gráfico
+        $categories = $points->map(function ($point) {
+            return $point->name_with_advance;
+        })->toArray();
+
+        $totalEffortPoints = $points->map(function ($point) {
+            return $point->total_effort_points;
+        })->toArray();
+
         return view('livewire.effort-points.effort-points', [
             'categories' => $categories,
             'series' => $series,
-            'days_filtred' => $days_filtred,
+            'totalEffortPoints' => $totalEffortPoints,
         ]);
     }
 
@@ -143,9 +176,9 @@ class EffortPoints extends Component
         // filtro
         $this->startDate = $startDate->format('Y-m-d');
         $this->endDate = $endDate->format('Y-m-d');
-        // input en vista
+        // Actualizar el rango de fechas
         $this->dateRange = $dateRange;
-        // Emitir un evento para forzar la actualización de la vista
-        $this->emit('dateUpdated');
+        // Forzar actualización de la vista
+        $this->emit('updateChart');
     }
 }
--- resources/views/livewire/effort-points/effort-points.blade.php
+++ resources/views/livewire/effort-points/effort-points.blade.php
@@ -3,9 +3,10 @@
         <div class="flex gap-2 text-sm md:flex-row lg:text-base">
             <div class="mb-2 inline-flex h-10 w-2/6 bg-transparent px-2 md:mx-3 md:px-0">
                 <label class="inputs" wire:ignore>
-                    <div class="flex inline-block">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
-                            stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+                    <div class="inline-block flex">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
+                            fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
+                            stroke-linejoin="round"
                             class="icon icon-tabler icons-tabler-outline icon-tabler-calendar-week mr-2">
                             <path stroke="none" d="M0 0h24v24H0z" fill="none" />
                             <path d="M4 7a2 2 0 0 1 2 -2h12a2 2 0 0 1 2 2v12a2 2 0 0 1 -2 2h-12a2 2 0 0 1 -2 -2v-12z" />
@@ -18,37 +19,26 @@
                         </svg>
                         <input disabled type="text" id="daterange" class="h-0 w-0 border-none bg-gray-100 p-0"
                             wire:model="dateRange" onchange="Livewire.emit('setDate', this.value)">
-                        <span class="text-base" id="spanDate">{{ $startDate }} - {{ $endDate }}</span>
+                        <span class="text-base" id="spanDate"></span>
                     </div>
                 </label>
             </div>
             <div class="mb-2 inline-flex h-12 w-2/6 bg-transparent px-2 md:mx-3 md:px-0">
-                Qty días. {{ $days_filtred }}
+                Qty días. {{ $qty }}
             </div>
         </div>
         <div id="chart" class="mt-5"></div>
     </div>
     @push('js')
         <script>
-            // FILTRO FECHA
             document.addEventListener('livewire:load', function() {
-                // // Obtener el día actual
-                // var today = moment();
-                // // Obtener el día de la semana (0: Domingo, 1: Lunes, ..., 6: Sábado)
-                // var dayOfWeek = today.day();
-                // // Calcular la fecha de inicio de la semana (lunes)
-                // var startOfWeek = moment().subtract(dayOfWeek - 1, 'days');
-                // // Calcular la fecha de fin de la semana (domingo)
-                // var endOfWeek = moment().add(7 - dayOfWeek, 'days');
-                // // Salida de las fechas
-                // var start = startOfWeek.format('YYYY-MM-DD');
-                // var end = endOfWeek.format('YYYY-MM-DD');
-                 // Obtener el primer y último día del mes actual
-                var startOfMonth = moment().startOf('month');
-                var endOfMonth = moment().endOf('month');
-                // Formatear las fechas en formato 'YYYY-MM-DD'
-                var start = startOfMonth.format('YYYY-MM-DD');
-                var end = endOfMonth.format('YYYY-MM-DD');
+                // FILTRO FECHA
+                var today = moment();
+                var dayOfWeek = today.day();
+                var startOfWeek = moment().subtract(dayOfWeek - 1, 'days');
+                var endOfWeek = moment().add(7 - dayOfWeek, 'days');
+                var start = startOfWeek.format('YYYY-MM-DD');
+                var end = endOfWeek.format('YYYY-MM-DD');
 
                 $('#daterange').daterangepicker({
                     locale: {
@@ -63,96 +53,119 @@
                         'Últimos 7 días': [moment().subtract(6, 'days'), moment()],
                         'Últimos 30 días': [moment().subtract(29, 'days'), moment()],
                         'Este mes': [moment().startOf('month'), moment().endOf('month')],
-                        'Mes pasado': [moment().subtract(1, 'month').startOf('month'), moment().subtract(1, 'month').endOf('month')],
+                        'Mes pasado': [moment().subtract(1, 'month').startOf('month'), moment().subtract(1,
+                            'month').endOf('month')]
                     }
                 }, function(start, end, label) {
                     $('#daterange').val(start.format('YYYY-MM-DD') + ' - ' + end.format('YYYY-MM-DD'));
                     @this.set('dateRange', start.format('YYYY-MM-DD') + ' - ' + end.format('YYYY-MM-DD'));
                 });
-                // Establecer el valor inicial del input
+
                 $('#spanDate').html(start + ' - ' + end);
-                // Actualizar el span al seleccionar un nuevo rango de fechas
+
                 $('#daterange').on('apply.daterangepicker', function(ev, picker) {
-                    $('#spanDate').html(picker.startDate.format('YYYY-MM-DD') + ' - ' + picker.endDate.format('YYYY-MM-DD'));
-                    Livewire.emit('setDate', picker.startDate.format('YYYY-MM-DD') + ' - ' + picker.endDate.format('YYYY-MM-DD'));
+                    $('#spanDate').html(picker.startDate.format('YYYY-MM-DD') + ' - ' + picker.endDate.format(
+                        'YYYY-MM-DD'));
+                    Livewire.emit('setDate', picker.startDate.format('YYYY-MM-DD') + ' - ' + picker.endDate
+                        .format('YYYY-MM-DD'));
                 });
-            });
-            // GRAFICA
-            document.addEventListener('livewire:load', function() {
-                var options = {
-                    series: @json($series),
-                    chart: {
-                        type: 'bar',
-                        height: 'auto',
-                        stacked: true,
-                    },
-                    colors: ['rgb(77, 124, 15)', 'rgb(250, 204, 21)', 'rgb(220, 38, 38)', 'rgb(59, 130, 246)',
-                        'rgb(243, 244, 246)'
-                    ], // Colores personalizados
-                    plotOptions: {
-                        bar: {
-                            horizontal: true,
-                            dataLabels: {
-                                position: 'top',
-                                total: {
-                                    enabled: true,
-                                    offsetX: 0,
-                                    style: {
-                                        fontSize: '13px',
-                                        fontWeight: 900,
-                                        color: function({
-                                            seriesIndex
-                                        }) {
-                                            return seriesIndex === 4 ? '#000' :
-                                            '#fff'; // Color de texto negro para 'Asignar', blanco para otros
+
+                // GRAFICA
+                var chart;
+                function renderChart(series, categories, totalEffortPoints) {
+                    if (chart) {
+                        chart.destroy();
+                    }
+
+                    var options = {
+                        series: series,
+                        chart: {
+                            type: 'bar',
+                            height: 'auto',
+                            stacked: true,
+                        },
+                        colors: ['rgb(77, 124, 15)', 'rgb(250, 204, 21)', 'rgb(220, 38, 38)', 'rgb(59, 130, 246)',
+                            'rgb(243, 244, 246)'
+                        ],
+                        plotOptions: {
+                            bar: {
+                                horizontal: true,
+                                dataLabels: {
+                                    position: 'top',
+                                    total: {
+                                        enabled: true,
+                                        formatter: function(val, opt) {
+                                            return totalEffortPoints[opt.dataPointIndex];
+                                        },
+                                        offsetX: 0,
+                                        style: {
+                                            fontSize: '13px',
+                                            fontWeight: 900,
+                                            color: function({
+                                                seriesIndex
+                                            }) {
+                                                return seriesIndex === 4 ? '#000' : '#fff';
+                                            }
                                         }
                                     }
                                 }
                             }
-                        }
-                    },
-                    dataLabels: {
-                        enabled: true,
-                        style: {
-                            colors: ['#fff', '#fff', '#fff', '#fff', '#000'] // Colores del texto para cada serie
-                        }
-                    },
-                    stroke: {
-                        width: 1,
-                        colors: ['#fff']
-                    },
-                    xaxis: {
-                        categories: @json($categories),
-                        labels: {
-                            formatter: function(val) {
-                                return val
-                            }
-                        }
-                    },
-                    yaxis: {
-                        title: {
-                            text: undefined
                         },
-                    },
-                    tooltip: {
-                        y: {
-                            formatter: function(val) {
-                                return val
+                        dataLabels: {
+                            enabled: true,
+                            style: {
+                                colors: ['#fff', '#fff', '#fff', '#fff', '#000']
                             }
+                        },
+                        stroke: {
+                            width: 1,
+                            colors: ['#fff']
+                        },
+                        xaxis: {
+                            categories: categories,
+                            labels: {
+                                formatter: function(val) {
+                                    return val;
+                                }
+                            }
+                        },
+                        yaxis: {
+                            title: {
+                                text: undefined
+                            }
+                        },
+                        tooltip: {
+                            y: {
+                                formatter: function(val) {
+                                    return val;
+                                }
+                            }
+                        },
+                        fill: {
+                            opacity: 1
+                        },
+                        legend: {
+                            position: 'top',
+                            horizontalAlign: 'left',
+                            offsetX: 40
                         }
-                    },
-                    fill: {
-                        opacity: 1
-                    },
-                    legend: {
-                        position: 'top',
-                        horizontalAlign: 'left',
-                        offsetX: 40
-                    }
-                };
+                    };
 
-                var chart = new ApexCharts(document.querySelector("#chart"), options);
-                chart.render();
+                    chart = new ApexCharts(document.querySelector("#chart"), options);
+                    chart.render();
+                }
+
+                Livewire.on('updateChart', function() {
+                    var series = @json($series);
+                    var categories = @json($categories);
+                    var totalEffortPoints = @json($totalEffortPoints);
+                    renderChart(series, categories, totalEffortPoints);
+                });
+
+                var initialSeries = @json($series);
+                var initialCategories = @json($categories);
+                var initialTotalEffortPoints = @json($totalEffortPoints);
+                renderChart(initialSeries, initialCategories, initialTotalEffortPoints);
             });
         </script>
     @endpush
