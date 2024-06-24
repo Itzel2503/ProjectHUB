6df1cd503d1f860dde7226c5bd0e0053ddb67dcd
Programacion <itzel.d.cruz.aguilar@hotmail.com>
2024-06-24 12:46:23 -0600

    prioridad actividades/reportes admins

--- resources/views/livewire/activities-reports/activities-reports.blade.php
+++ resources/views/livewire/activities-reports/activities-reports.blade.php
@@ -716,15 +716,19 @@
                                             </p>
                                         </div>
                                         <div class="flex flex-row">
-                                            <div class="my-2 w-auto rounded-md px-3 text-center font-semibold">
-                                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"
-                                                    viewBox="0 0 24 24" fill="currentColor"
-                                                    class="icon icon-tabler icons-tabler-filled icon-tabler-circle @if ($task->priority == 'Alto') text-red-500 @endif @if ($task->priority == 'Medio') text-yellow-400 @endif @if ($task->priority == 'Bajo') text-blue-500 @endif">
-                                                    <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                                                    <path
-                                                        d="M7 3.34a10 10 0 1 1 -4.995 8.984l-.005 -.324l.005 -.324a10 10 0 0 1 4.995 -8.336z" />
-                                                </svg>
-                                            </div>
+                                            @if ($task->priority)
+                                                <div class="my-2 w-auto rounded-md px-3 text-center font-semibold">
+                                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"
+                                                        viewBox="0 0 24 24" fill="currentColor"
+                                                        class="icon icon-tabler icons-tabler-filled icon-tabler-circle @if ($task->priority == 'Alto') text-red-500 @elseif ($task->priority == 'Medio') text-yellow-400 @elseif ($task->priority == 'Bajo') text-blue-500 @endif">
+                                                        <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                                                        <path
+                                                            d="M7 3.34a10 10 0 1 1 -4.995 8.984l-.005 -.324l.005 -.324a10 10 0 0 1 4.995 -8.336z" />
+                                                    </svg>
+                                                </div>
+                                            @else
+                                                <div class="w-12"></div>
+                                            @endif
                                             <p class="my-auto text-left text-xs font-semibold">
                                                 {{ $task->title }}
                                             </p>
