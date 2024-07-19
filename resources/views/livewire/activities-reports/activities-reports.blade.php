918443a2d3d33869a1b02b6b143c10fe19c91710
Programacion <itzel.d.cruz.aguilar@hotmail.com>
2024-07-19 12:51:46 -0600

    ocultar icon de msn en actividades/Reportes

--- resources/views/livewire/activities-reports/activities-reports.blade.php
+++ resources/views/livewire/activities-reports/activities-reports.blade.php
@@ -11,14 +11,14 @@
             <button wire:click="setActiveTab('actividades')"
                 class="border-primaryColor @if ($activeTab === 'actividades') rounded-t-lg border-x-2 border-t-2 @else border-b-2 @endif whitespace-nowrap px-3 pb-4 text-sm font-medium">
                 Actividades
-                @if ($totalMessagesCount > 0)
+                {{-- @if ($totalMessagesCount > 0)
                     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-message text-red-600">
                         <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
                         <path d="M8 9h8"/>
                         <path d="M8 13h6"/>
                         <path d="M18 4a3 3 0 0 1 3 3v8a3 3 0 0 1 -3 3h-5l-5 3v-3h-2a3 3 0 0 1 -3 -3v-8a3 3 0 0 1 3 -3h12z"/>
                     </svg>
-                @endif
+                @endif --}}
             </button>
             <button wire:click="setActiveTab('reportes')"
                 class="border-primaryColor @if ($activeTab === 'reportes') rounded-t-lg border-x-2 border-t-2 @else border-b-2 @endif whitespace-nowrap px-3 pb-4 text-sm font-medium">
